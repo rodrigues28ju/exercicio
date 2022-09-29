b33b8effbe67053dc6e4255969ec29cc0d1bc243
Juliana Rodrigues Nogueira <114073784+rodrigues28ju@users.noreply.github.com>
2022-09-29 17:19:21 -0300

    atividade 1 feita

---
+++ trabalho/variaveis.js
@@ -0,0 +1,25 @@
+var nome = "Juliana";
+var idade = 27  + " anos ";
+
+console.log(nome,idade);
+
+var nomeSecundario = "Maria Fernanda";
+var idadeSecundario = 21 + " anos ";
+
+console.log( nomeSecundario,idadeSecundario);
+
+var comida = "feijao";
+console.log (typeof comida);
+
+var numero = 44;
+console.log (typeof numero);
+
+var symbol = {};
+console.log (typeof symbol);
+
+var undefido = 
+console.log(typeof undefido);
+
+var nulo = document.querySelector(".info");
+
+console.log (nulo); 
